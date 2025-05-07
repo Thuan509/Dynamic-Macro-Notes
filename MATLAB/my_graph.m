842c6ca35f8e64baa588cab071ff25e1769068d1
Thu An Do <an.do.210178@student.fulbright.edu.vn>
2025-05-07 23:46:18 +0700

    May7: update 1

--- MATLAB/my_graph.m
+++ MATLAB/my_graph.m
@@ -14,7 +14,7 @@ classdef my_graph
     methods(Static)
         %% Plot value and policy functions.
         
-        function [] = plot_policy(par,sol)
+        function [] = plot_policy(par,sol, figout)
             %% Plot discrete choice policy function.
 
             close all
@@ -29,6 +29,7 @@ classdef my_graph
                 ylabel({'$e_{t}$'},'Interpreter','latex') 
                 zlabel({'$o_{t}$'},'Interpreter','latex') 
             title('Occupational Choice (0=Self-Emplopyed, 1=Worker)','Interpreter','latex')
+            saveas(gcf, fullfile(figout, 'opol1_1.png'))
             
             figure(2)
             
@@ -37,6 +38,7 @@ classdef my_graph
                 ylabel({'$e_{t}$'},'Interpreter','latex') 
                 zlabel({'$c_{t}$'},'Interpreter','latex') 
             title('Consumption Policy Function (Worker)','Interpreter','latex')
+            saveas(gcf, fullfile(figout, 'cpol1_1.png'))
             
             figure(3)
             
@@ -45,6 +47,7 @@ classdef my_graph
                 ylabel({'$e_{t}$'},'Interpreter','latex') 
                 zlabel({'$c_{t}$'},'Interpreter','latex') 
             title('Consumption Policy Function (Self-Employed)','Interpreter','latex')
+            saveas(gcf, fullfile(figout, 'cpol1_2.png'))
             
             figure(4)
             
@@ -53,6 +56,7 @@ classdef my_graph
                 ylabel({'$e_{t}$'},'Interpreter','latex') 
                 zlabel({'$v_{t}$'},'Interpreter','latex') 
             title('Value Function (Worker)','Interpreter','latex')
+            saveas(gcf, fullfile(figout, 'vpol1_1.png'))
             
             figure(5)
             
@@ -61,6 +65,7 @@ classdef my_graph
                 ylabel({'$e_{t}$'},'Interpreter','latex') 
                 zlabel({'$v_{t}$'},'Interpreter','latex') 
             title('Value Function (Self-Employed)','Interpreter','latex')
+            saveas(gcf, fullfile(figout, 'vpol1_2.png'))
             
             figure(6)
             
@@ -69,6 +74,7 @@ classdef my_graph
                 ylabel({'$e_{t}$'},'Interpreter','latex') 
                 zlabel({'$v_{t}$'},'Interpreter','latex') 
             title('Value Function (Overall)','Interpreter','latex')
+            saveas(gcf, fullfile(figout, 'vpol1_2.png'))
             
             figure(7)
             
@@ -77,6 +83,7 @@ classdef my_graph
                 ylabel({'$e_{t}$'},'Interpreter','latex') 
                 zlabel({'$a_{t}$'},'Interpreter','latex') 
             title('Savings Policy Function (Worker)','Interpreter','latex')
+            saveas(gcf, fullfile(figout, 'apol1_1.png'))
             
             figure(8)
             
@@ -85,6 +92,7 @@ classdef my_graph
                 ylabel({'$e_{t}$'},'Interpreter','latex') 
                 zlabel({'$a_{t}$'},'Interpreter','latex') 
             title('Savings Policy Function (Self-Employed)','Interpreter','latex')
+            saveas(gcf, fullfile(figout, 'apol1_2.png'))
             
             figure(9)
             
@@ -93,6 +101,7 @@ classdef my_graph
                 ylabel({'$e_{t}$'},'Interpreter','latex') 
                 zlabel({'$\bar{K}_{t}$'},'Interpreter','latex') 
             title('Capital Rental Limit','Interpreter','latex')
+            saveas(gcf, fullfile(figout, 'kpol1_1.png'))
             
             figure(10)
             
@@ -101,6 +110,7 @@ classdef my_graph
                 ylabel({'$e_{t}$'},'Interpreter','latex') 
                 zlabel({'$\pi_{t}$'},'Interpreter','latex') 
             title('Profit Function','Interpreter','latex')
+            saveas(gcf, fullfile(figout, 'ppol1_1.png'))
             
         end
 
